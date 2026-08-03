473f69104d1137c2aa6a8b426d405f47900ed5e9
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz@farmaenlace.com>
2026-08-03 15:33:21 -0500

    Actualización de librerías, mejora en las graficas del dashboard

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","favicon-16x16.png","favicon-32x32.png","favicon.ico","favicon.png","icono01.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.CAjYbBC7.js",app:"_app/immutable/entry/app.CXjRf-8g.js",imports:["_app/immutable/entry/start.CAjYbBC7.js","_app/immutable/chunks/rV8-eTAE.js","_app/immutable/chunks/CCnce0PM.js","_app/immutable/chunks/DLKkhhwc.js","_app/immutable/entry/app.CXjRf-8g.js","_app/immutable/chunks/CCnce0PM.js","_app/immutable/chunks/Bjw7MXHe.js","_app/immutable/chunks/B0vI5HJ-.js","_app/immutable/chunks/DLKkhhwc.js","_app/immutable/chunks/iA1A_eTV.js","_app/immutable/chunks/BpTGr0Fa.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.B-9UHfzt.js",app:"_app/immutable/entry/app.C10yWZlc.js",imports:["_app/immutable/entry/start.B-9UHfzt.js","_app/immutable/chunks/CPuKFvuf.js","_app/immutable/chunks/CCnce0PM.js","_app/immutable/chunks/DLKkhhwc.js","_app/immutable/entry/app.C10yWZlc.js","_app/immutable/chunks/CCnce0PM.js","_app/immutable/chunks/Bjw7MXHe.js","_app/immutable/chunks/B0vI5HJ-.js","_app/immutable/chunks/DLKkhhwc.js","_app/immutable/chunks/iA1A_eTV.js","_app/immutable/chunks/BpTGr0Fa.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
---
+++ www/_app/immutable/entry/start.B-9UHfzt.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/CPuKFvuf.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.B-9UHfzt.js.map
